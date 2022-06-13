a51d77284efda89db8fa5d68c3c1202ebdca78b3
AquexTheSeal <sheenix15@gmail.com>
2022-06-13 21:34:09 +0800

    experimenting non-facing particles + some access transformer entries.

--- src/main/java/com/aquextheseal/woe/particle/CrystalSparkParticle.java
+++ src/main/java/com/aquextheseal/woe/particle/CrystalSparkParticle.java
@@ -1,14 +1,11 @@
 package com.aquextheseal.woe.particle;
 
+import com.aquextheseal.woe.util.MERenderUtil;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Quaternion;
-import com.mojang.math.Vector3f;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraft.util.Mth;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -49,34 +46,7 @@ public class CrystalSparkParticle extends TextureSheetParticle {
 
     @Override
     public void render(VertexConsumer pBuffer, Camera pRenderInfo, float pPartialTicks) {
-        Vec3 vec3 = pRenderInfo.getPosition();
-        float f = (float)(Mth.lerp(pPartialTicks, this.xo, this.x) - vec3.x());
-        float f1 = (float)(Mth.lerp(pPartialTicks, this.yo, this.y) - vec3.y());
-        float f2 = (float)(Mth.lerp(pPartialTicks, this.zo, this.z) - vec3.z());
-        Quaternion quaternion;
-
-        quaternion = new Quaternion(0.0F, 0.0F, 0.0F, 1.0F);
-        quaternion.mul(Vector3f.XP.rotation(1.6F));
-
-        Vector3f vector3f1 = new Vector3f(-1.0F, -1.0F, 0.0F);
-        vector3f1.transform(quaternion);
-        Vector3f[] avector3f = new Vector3f[]{new Vector3f(-1.0F, -1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(1.0F, -1.0F, 0.0F)};
-        float f4 = this.getQuadSize(pPartialTicks);
-        for(int i = 0; i < 4; ++i) {
-            Vector3f vector3f = avector3f[i];
-            vector3f.transform(quaternion);
-            vector3f.mul(f4);
-            vector3f.add(f, f1, f2);
-        }
-        float f7 = this.getU0();
-        float f8 = this.getU1();
-        float f5 = this.getV0();
-        float f6 = this.getV1();
-        int j = this.getLightColor(pPartialTicks);
-        pBuffer.vertex(avector3f[0].x(), avector3f[0].y(), avector3f[0].z()).uv(f8, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
-        pBuffer.vertex(avector3f[1].x(), avector3f[1].y(), avector3f[1].z()).uv(f8, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
-        pBuffer.vertex(avector3f[2].x(), avector3f[2].y(), avector3f[2].z()).uv(f7, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
-        pBuffer.vertex(avector3f[3].x(), avector3f[3].y(), avector3f[3].z()).uv(f7, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
+        MERenderUtil.renderNonFacingParticle(this, pBuffer, pRenderInfo, pPartialTicks);
     }
 
     @OnlyIn(Dist.CLIENT)
---
+++ src/main/java/com/aquextheseal/woe/util/MERenderUtil.java
@@ -0,0 +1,50 @@
+package com.aquextheseal.woe.util;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.Camera;
+import net.minecraft.client.particle.SingleQuadParticle;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
+
+public class MERenderUtil {
+
+    /**
+     * Override Particle#render with this method if you want to use it.
+     */
+    public static void renderNonFacingParticle(SingleQuadParticle particle, VertexConsumer pBuffer, Camera pRenderInfo, float pPartialTicks, float xRot) {
+        Vec3 vec3 = pRenderInfo.getPosition();
+        float f = (float)(Mth.lerp(pPartialTicks, particle.xo, particle.x) - vec3.x());
+        float f1 = (float)(Mth.lerp(pPartialTicks, particle.yo, particle.y) - vec3.y());
+        float f2 = (float)(Mth.lerp(pPartialTicks, particle.zo, particle.z) - vec3.z());
+        Quaternion quaternion;
+
+        quaternion = new Quaternion(0.0F, 0.0F, 0.0F, 1.0F);
+        quaternion.mul(Vector3f.XP.rotation(xRot));
+
+        Vector3f vector3f1 = new Vector3f(-1.0F, -1.0F, 0.0F);
+        vector3f1.transform(quaternion);
+        Vector3f[] avector3f = new Vector3f[]{new Vector3f(-1.0F, -1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(1.0F, -1.0F, 0.0F)};
+        float f4 = particle.getQuadSize(pPartialTicks);
+        for(int i = 0; i < 4; ++i) {
+            Vector3f vector3f = avector3f[i];
+            vector3f.transform(quaternion);
+            vector3f.mul(f4);
+            vector3f.add(f, f1, f2);
+        }
+        float f7 = particle.getU0();
+        float f8 = particle.getU1();
+        float f5 = particle.getV0();
+        float f6 = particle.getV1();
+        int j = particle.getLightColor(pPartialTicks);
+        pBuffer.vertex(avector3f[0].x(), avector3f[0].y(), avector3f[0].z()).uv(f8, f6).color(particle.rCol, particle.gCol, particle.bCol, particle.alpha).uv2(j).endVertex();
+        pBuffer.vertex(avector3f[1].x(), avector3f[1].y(), avector3f[1].z()).uv(f8, f5).color(particle.rCol, particle.gCol, particle.bCol, particle.alpha).uv2(j).endVertex();
+        pBuffer.vertex(avector3f[2].x(), avector3f[2].y(), avector3f[2].z()).uv(f7, f5).color(particle.rCol, particle.gCol, particle.bCol, particle.alpha).uv2(j).endVertex();
+        pBuffer.vertex(avector3f[3].x(), avector3f[3].y(), avector3f[3].z()).uv(f7, f6).color(particle.rCol, particle.gCol, particle.bCol, particle.alpha).uv2(j).endVertex();
+    }
+
+    public static void renderNonFacingParticle(SingleQuadParticle particle, VertexConsumer pBuffer, Camera pRenderInfo, float pPartialTicks) {
+        renderNonFacingParticle(particle, pBuffer, pRenderInfo, pPartialTicks, 1.6F);
+    }
+}
